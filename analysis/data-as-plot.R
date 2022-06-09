564ae63bbbcc81c9c5c00a0c0f08a7239dbf6e8f
jose <jcmc@fe.up.pt>
2022-06-09 09:50:18 +0100

    Attempt to plot missing data points.

--- analysis/data-as-plot.R
+++ analysis/data-as-plot.R
@@ -144,7 +144,7 @@ df <- df[df$'used_qpl' == 'Yes', ]
 # FIXME Verify why returning only 203 lines instead of 208 - Problem in the aggregate function
 # FIXME X axis scale in percentual is wrong
 plot_label('What is your age?')
-agg <- aggregate(x=. ~ timestamp + age, data=df, FUN=length)
+agg <- aggregate(x=country ~ timestamp + age, data=df, FUN=length)
 make_bar_plot(agg, x='age', TRUE)
 #make_pie_plot(agg, fill='age', FALSE)
 remove(agg)
@@ -155,7 +155,7 @@ remove(agg)
 # FIXME Verify why returning only 203 lines instead of 208 - Problem in the aggregate function
 # FIXME X axis scale in percentual is wrong
 plot_label('Where do you live? (Country)')
-agg <- aggregate(x=. ~ timestamp + country, data=df, FUN=length)
+agg <- aggregate(x=age ~ timestamp + country, data=df, FUN=length)
 make_bar_plot(agg, x='country', TRUE)
 #make_pie_plot(agg, fill='country', FALSE)
 remove(agg)
@@ -165,7 +165,7 @@ remove(agg)
 #
 # FIXME Verify why returning only 203 lines instead of 208 - Problem in the aggregate function
 plot_label('Which of the following describe you?')
-agg <- aggregate(x=. ~ timestamp + gender, data=df, FUN=length)
+agg <- aggregate(x=country ~ timestamp + gender, data=df, FUN=length)
 #make_bar_plot(agg, x='gender', FALSE)
 make_pie_plot(agg, fill='gender', TRUE)
 remove(agg)
@@ -176,7 +176,7 @@ remove(agg)
 # FIXME Verify why returning only 203 lines instead of 208 - Problem in the aggregate function
 # FIXME X axis scale in percentual is wrong
 plot_label('How many years have you been coding?')
-agg <- aggregate(x=. ~ timestamp + years_coding, data=df, FUN=length)
+agg <- aggregate(x=country ~ timestamp + years_coding, data=df, FUN=length)
 make_bar_plot(agg, x='years_coding', TRUE)
 #make_pie_plot(agg, fill='years_coding', FALSE)
 remove(agg)
@@ -187,7 +187,7 @@ remove(agg)
 # FIXME Verify why returning only 203 lines instead of 208 - Problem in the aggregate function
 # FIXME X axis scale in percentual is wrong
 plot_label('How many years have you coded professionally (as a part\nof your work)?')
-agg <- aggregate(x=. ~ timestamp + years_coded_professionally, data=df, FUN=length)
+agg <- aggregate(x=country ~ timestamp + years_coded_professionally, data=df, FUN=length)
 make_bar_plot(agg, x='years_coded_professionally', TRUE)
 #make_pie_plot(agg, fill='years_coded_professionally', FALSE)
 remove(agg)
@@ -202,7 +202,7 @@ plot_label('How did you learn to code?')
 agg <- as.data.frame(df %>% separate_rows(learned_code, sep=';'))
 # Replace open-answers with 'Other'
 agg$'learned_code'[agg$'learned_code' %!in% c('Books / Physical media', 'Coding Bootcamp', 'Colleague', 'Friend or family member', 'Online Courses or Certification', 'Online Forum', 'Other online resources (videos, blogs, etc)', 'School')] <- 'Other'
-agg <- aggregate(x=. ~ timestamp + learned_code, data=agg, FUN=length)
+agg <- aggregate(x=country ~ timestamp + learned_code, data=agg, FUN=length)
 make_bar_plot(agg, x='learned_code', TRUE)
 #make_pie_plot(agg, fill='learned_code', FALSE)
 remove(agg)
@@ -217,7 +217,7 @@ plot_label('What are the most used programming, scripting, and markup\n language
 agg <- as.data.frame(df %>% separate_rows(used_programming_language, sep=';'))
 # Replace open-answers with 'Other'
 agg$'used_programming_language'[agg$'used_programming_language' %!in% c('Assembly', 'Bash', 'C', 'Classic Visual Basic', 'COBOL', 'C++', 'C#', 'Delphi/Object Pascal', 'Fortran', 'F#', 'Go', 'Groovy', 'Haskell', 'Java', 'JavaScrpit', 'Julia', 'Lisp', 'Matlab', 'ML', 'Objective-C', 'Pascal', 'Perl', 'pGCL', 'PHP', 'PowerShell', 'Prolog', 'Python', 'Ruby', 'SQL', 'Standard ML', 'Swift', 'Visual Basic', 'Visual C++')] <- 'Other'
-agg <- aggregate(x=. ~ timestamp + used_programming_language, data=agg, FUN=length)
+agg <- aggregate(x=country ~ timestamp + used_programming_language, data=agg, FUN=length)
 make_bar_plot(agg, x='used_programming_language', TRUE)
 #make_pie_plot(agg, fill='used_programming_language', FALSE)
 remove(agg)
@@ -227,7 +227,7 @@ remove(agg)
 #
 # FIXME Verify why returning only 203 lines instead of 208 - Problem in the aggregate function
 plot_label('What is your level of knowledge in Quantum Physics?')
-agg <- aggregate(x=. ~ timestamp + level_quantum_physics, data=df, FUN=length)
+agg <- aggregate(x=country ~ timestamp + level_quantum_physics, data=df, FUN=length)
 #make_bar_plot(agg, x='level_quantum_physics', TRUE)
 make_pie_plot(agg, fill='level_quantum_physics', TRUE)
 remove(agg)
@@ -241,7 +241,7 @@ plot_label('Where did you learn Quantum Physics?')
 agg <- as.data.frame(df %>% separate_rows(learned_quantum_physics, sep=';'))
 # Replace open-answers with 'Other'
 agg$'learned_quantum_physics'[agg$'learned_quantum_physics' %!in% c('Books', 'Online Course', 'Search Sites', 'University', 'Work')] <- 'Other'
-agg <- aggregate(x=. ~ timestamp + learned_quantum_physics, data=agg, FUN=length)
+agg <- aggregate(x=country ~ timestamp + learned_quantum_physics, data=agg, FUN=length)
 make_bar_plot(agg, x='learned_quantum_physics', TRUE)
 #make_pie_plot(agg, fill='learned_quantum_physics', TRUE)
 remove(agg)
@@ -253,7 +253,7 @@ remove(agg)
 # FIXME Y axis Labels - too large
 # FIXME X axis scale in percentual is wrong
 plot_label('Which of the following best describes the highest level\nof education that you have completed?')
-agg <- aggregate(x=. ~ timestamp + level_education, data=df, FUN=length)
+agg <- aggregate(x=country ~ timestamp + level_education, data=df, FUN=length)
 make_bar_plot(agg, x='level_education', TRUE)
 #make_pie_plot(agg, fill='level_education', FALSE)
 remove(agg)
@@ -268,7 +268,7 @@ plot_label('If you have completed a major, what is the subject?')
 agg <- as.data.frame(df %>% separate_rows(major, sep=';'))
 # Replace open-answers with 'Other'
 agg$'major'[agg$'major' %!in% c('Art / Humanities', 'Computer Science', 'Economics', 'Software Engineering', 'Math', 'Other Engineering', 'Physics', 'Social Sciences')] <- 'Other'
-agg <- aggregate(x=. ~ timestamp + major, data=agg, FUN=length)
+agg <- aggregate(x=country ~ timestamp + major, data=agg, FUN=length)
 make_bar_plot(agg, x='major', TRUE)
 #make_pie_plot(agg, fill='major', FALSE)
 remove(agg)
@@ -284,7 +284,7 @@ plot_label('Which of the following describes your current job?')
 agg <- as.data.frame(df %>% separate_rows(job, sep=';'))
 # Replace open-answers with 'Other'
 agg$'job'[agg$'job' %!in% c('Academic researcher', 'Architect', 'Business Analyst', 'CIO / CEO / CTO', 'DBA (Database Administrator)', 'Data Analyst / Data Engineer/ Data Scientist', 'Developer Advocate', 'Developer / Programmer / Software Engineer', 'DevOps Engineer / Infrastructure Developer', 'Instructor / Teacher / Tutor', 'Marketing Manager', 'Product Manager', 'Project Manager', 'Scientist / Researcher', 'Student', 'Systems Analyst', 'Team Lead', 'Technical Support', 'Technical Writer', 'Tester / QA Engineer', 'UX / UI Designer')] <- 'Other'
-agg <- aggregate(x=. ~ timestamp + job, data=agg, FUN=length)
+agg <- aggregate(x=country ~ timestamp + job, data=agg, FUN=length)
 make_bar_plot(agg, x='job', TRUE)
 #make_pie_plot(agg, fill='job', FALSE)
 remove(agg)
@@ -299,7 +299,7 @@ plot_label('Where and how did you learn Quantum Programming Languages?')
 agg <- as.data.frame(df %>% separate_rows(learned_qpl, sep=';'))
 # Replace open-answers with 'Other'
 agg$'learned_qpl'[agg$'learned_qpl' %!in% c('Books', 'Language documentation', 'University', 'Online Course', 'Online Forums', 'Search Sites', 'Work')] <- 'Other'
-agg <- aggregate(x=. ~ timestamp + learned_qpl, data=agg, FUN=length)
+agg <- aggregate(x=country ~ timestamp + learned_qpl, data=agg, FUN=length)
 make_bar_plot(agg, x='learned_qpl', TRUE)
 #make_pie_plot(agg, fill='learned_qpl', FALSE)
 remove(agg)
@@ -309,7 +309,7 @@ remove(agg)
 #
 # FIXME Verify why returning only 203 lines instead of 208 - Problem in the aggregate function
 plot_label('How many years have you been coding using Quantum \nProgramming Languages?')
-agg <- aggregate(x=. ~ timestamp + years_coded_qpls, data=df, FUN=length)
+agg <- aggregate(x=country ~ timestamp + years_coded_qpls, data=df, FUN=length)
 #make_bar_plot(agg, x='years_coded_qpls', FALSE)
 make_pie_plot(agg, fill='years_coded_qpls', TRUE)
 remove(agg)
@@ -319,7 +319,7 @@ remove(agg)
 #
 # FIXME Verify why returning only 203 lines instead of 208 - Problem in the aggregate function
 plot_label('How many years have you coded professionally using Quantum \nProgramming Languages (as a part of your work)?')
-agg <- aggregate(x=. ~ timestamp + years_coded_professionally_qpls, data=df, FUN=length)
+agg <- aggregate(x=country ~ timestamp + years_coded_professionally_qpls, data=df, FUN=length)
 #make_bar_plot(agg, x='years_coded_professionally_qpls', FALSE)
 make_pie_plot(agg, fill='years_coded_professionally_qpls', TRUE)
 remove(agg)
@@ -331,7 +331,7 @@ remove(agg)
 plot_label('What Quantum Programming Languages / frameworks have you \nbeen using and for how long?')
 # Convert dataframe from wide to long (row level), i.e., collapse a column with multiple values into multiple rows
 agg <- as.data.frame(df %>% separate_rows(used_qpls, sep=';'))
-agg <- aggregate(x=. ~ timestamp + used_qpls + used_qpls_value, data=agg, FUN=length)
+agg <- aggregate(x=country ~ timestamp + used_qpls + used_qpls_value, data=agg, FUN=length)
 agg <- agg[agg$'used_qpls_value' != '', ]
 #make_dodge_plot(agg, x='used_qpls', fill='used_qpls_value', legendsTitle='How long')
 
@@ -367,7 +367,7 @@ remove(agg)
 #
 # FIXME Verify why returning only 203 lines instead of 208 - Problem in the aggregate function
 plot_label('Which of the following is your primary Quantum Programming \nLanguage / framework?')
-agg <- aggregate(x=. ~ timestamp + primary_qpl, data=df, FUN=length)
+agg <- aggregate(x=country ~ timestamp + primary_qpl, data=df, FUN=length)
 make_bar_plot(agg, x='primary_qpl', TRUE)
 #make_pie_plot(agg, fill='primary_qpl', FALSE)
 remove(agg)
@@ -380,7 +380,7 @@ remove(agg)
 plot_label('In terms of ease, rate your primary Quantum Programming Language.')
 # Convert dataframe from wide to long (row level), i.e., collapse a column with multiple values into multiple rows
 agg <- as.data.frame(df %>% separate_rows(rate_primary_qpl, sep=';'))
-agg <- aggregate(x=. ~ timestamp + rate_primary_qpl + rate_primary_qpl_value, data=agg, FUN=length)
+agg <- aggregate(x=country ~ timestamp + rate_primary_qpl + rate_primary_qpl_value, data=agg, FUN=length)
 agg <- agg[agg$'rate_primary_qpl_value' != '', ]
 #make_dodge_plot(agg, x='rate_primary_qpl', fill='rate_primary_qpl_value', legendsTitle='')
 
@@ -418,7 +418,7 @@ plot_label('Which forums, e.g., to ask for help, search for examples, \ndo you u
 agg <- as.data.frame(df %>% separate_rows(forum, sep=';'))
 # Replace open-answers with 'Other'
 agg$'forum'[agg$'forum' %!in% c('Devtalk', 'Quantum Open Source Foundation', 'Slack', 'StackOverflow')] <- 'Other'
-agg <- aggregate(x=. ~ timestamp + forum, data=agg, FUN=length)
+agg <- aggregate(x=country ~ timestamp + forum, data=agg, FUN=length)
 make_bar_plot(agg, x='forum', TRUE)
 #make_pie_plot(agg, fill='forum', FALSE)
 remove(agg)
@@ -433,7 +433,7 @@ plot_label('Which Quantum Programming Languages / frameworks would you \nlike to
 agg <- as.data.frame(df %>% separate_rows(qpl_future, sep=';'))
 # Replace open-answers with 'Other'
 agg$'qpl_future'[agg$'qpl_future' %!in% c('Blackbird', 'Braket SDK', 'Cirq', 'Cove', 'cQASM', 'CQP (Communication Quantum Processes)', 'cQPL', 'Forest', 'Ket', 'LanQ', '𝐿𝐼𝑄𝑈𝑖|⟩', 'NDQFP', 'NDQJava', 'Ocean Software', 'OpenQASM', 'Orquestra', 'ProjectQ', 'Q Language', 'QASM (Quantum Macro Assembler)', 'QCL (Quantum Computation Language)', 'QDK (Quantum Development Kit)', 'QHAL', 'Qiskit', 'qGCL', 'QHaskell', 'QML', 'QPAlg (Quantum Process Algebra)', 'QPL and QFC', 'QSEL', 'QuaFL (DSL for quantum programming)', 'Quil', 'Quipper', 'Q#', '𝑄|𝑆𝐼⟩', 'Sabry\'s Language', 'Scaffold', 'Silq', 'Strawberry Fields', '𝜆𝑞 (Lambda Calculi)')] <- 'Other'
-agg <- aggregate(x=. ~ timestamp + qpl_future, data=agg, FUN=length)
+agg <- aggregate(x=country ~ timestamp + qpl_future, data=agg, FUN=length)
 make_bar_plot(agg, x='qpl_future', TRUE)
 #make_pie_plot(agg, fill='qpl_future', FALSE)
 remove(agg)
@@ -448,7 +448,7 @@ plot_label('Why would you like to work or try those languages / frameworks?')
 agg <- as.data.frame(df %>% separate_rows(why_like_try_qpl, sep=';'))
 # Replace open-answers with 'Other'
 agg$'why_like_try_qpl'[agg$'why_like_try_qpl' %!in% c('Heard about the language', 'Is part of a course about the language', 'Read an article about the language', 'Widely used', 'Other features')] <- 'Other'
-agg <- aggregate(x=. ~ timestamp + why_like_try_qpl, data=agg, FUN=length)
+agg <- aggregate(x=country ~ timestamp + why_like_try_qpl, data=agg, FUN=length)
 make_bar_plot(agg, x='why_like_try_qpl', TRUE)
 #make_pie_plot(agg, fill='why_like_try_qpl', FALSE)
 remove(agg)
@@ -462,7 +462,7 @@ plot_label('How do you use Quantum Programming Languages?')
 agg <- as.data.frame(df %>% separate_rows(how_use_qpl, sep=';'))
 # Replace open-answers with 'Other'
 agg$'how_use_qpl'[agg$'how_use_qpl' %!in% c('Use it for work', 'Use it for research', 'Like to learn')] <- 'Other'
-agg <- aggregate(x=. ~ timestamp + how_use_qpl, data=agg, FUN=length)
+agg <- aggregate(x=country ~ timestamp + how_use_qpl, data=agg, FUN=length)
 #make_bar_plot(agg, x='how_use_qpl', FALSE)
 make_pie_plot(agg, fill='how_use_qpl', TRUE)
 remove(agg)
@@ -472,7 +472,7 @@ remove(agg)
 #
 # FIXME Verify why returning only 203 lines instead of 208 - Problem in the aggregate function
 plot_label('Do you test your Quantum Programs?')
-agg <- aggregate(x=. ~ timestamp + do_test, data=df, FUN=length)
+agg <- aggregate(x=country ~ timestamp + do_test, data=df, FUN=length)
 #make_bar_plot(agg, x='do_test', FALSE)
 make_pie_plot(agg, fill='do_test', TRUE)
 remove(agg)
@@ -487,7 +487,7 @@ plot_label('How often do you test your Quantum Programs?')
 agg <- as.data.frame(df %>% separate_rows(how_often_test, sep=';'))
 # Replace open-answers with 'Other'
 agg$'how_often_test'[agg$'how_often_test' %!in% c('Before go to production', 'Every day', 'Every time you change the code')] <- 'Other'
-agg <- aggregate(x=. ~ timestamp + how_often_test, data=agg, FUN=length)
+agg <- aggregate(x=country ~ timestamp + how_often_test, data=agg, FUN=length)
 make_bar_plot(agg, x='how_often_test', TRUE)
 #make_pie_plot(agg, fill='how_often_test', TRUE)
 remove(agg)
@@ -497,7 +497,7 @@ remove(agg)
 #
 # FIXME Missing answers ( responses) - Problem in the aggregate function
 plot_label('How do you test your Quantum Programs?')
-agg <- aggregate(x=. ~ timestamp + how_test, data=df[df$'how_test' != '', ], FUN=length)
+agg <- aggregate(x=country ~ timestamp + how_test, data=df[df$'how_test' != '', ], FUN=length)
 #make_bar_plot(agg, x='how_test', FALSE)
 make_pie_plot(agg, fill='how_test', TRUE)
 remove(agg)
@@ -513,7 +513,7 @@ plot_label('What tools do you use to test your Quantum Programs?')
 agg <- as.data.frame(df %>% separate_rows(tools_test, sep=';'))
 # Replace open-answers with 'Other'
 agg$'tools_test'[agg$'tools_test' %!in% c('Cirq Simulator and Testing - cirq.testing (https://quantumai.google/cirq)', 'Forest using pytest (https://github.com/rigetti/forest-software)', 'MTQC - Mutation Testing for Quantum Computing (https://javpelle.github.io/MTQC/)', 'Muskit: A Mutation Analysis Tool for Quantum Software Testing (https://ieeexplore.ieee.org/document/9678563)', 'ProjectQ Simulator (https://arxiv.org/abs/1612.08091)', 'QDiff - Differential Testing of Quantum Software Stacks (https://ieeexplore.ieee.org/abstract/document/9678792)', 'QDK - xUnit (https://azure.microsoft.com/en-us/resources/development-kit/quantum-computing/)', 'Qiskit - QASM Simulator (https://qiskit.org/)', 'QuanFuzz - Fuzz Testing of Quantum Program (https://arxiv.org/abs/1810.10310)', 'Quito - A Coverage-Guided Test Generator for Quantum Programs (https://ieeexplore.ieee.org/abstract/document/9678798)', 'Straberry Fields using pytest (https://strawberryfields.ai/)')] <- 'Other'
-agg <- aggregate(x=. ~ timestamp + tools_test, data=agg, FUN=length)
+agg <- aggregate(x=country ~ timestamp + tools_test, data=agg, FUN=length)
 make_bar_plot(agg, x='tools_test', TRUE)
 #make_pie_plot(agg, fill='tools_test', FALSE)
 remove(agg)
@@ -523,7 +523,7 @@ remove(agg)
 #
 # FIXME Verify why returning only 203 lines instead of 208 - Problem in the aggregate function
 plot_label('In your opinion, do you think there are too many or too few \nQuantum Programming Languages? Why?')
-agg <- aggregate(x=. ~ timestamp + why_too_many_qpl_resp, data=df, FUN=length)
+agg <- aggregate(x=country ~ timestamp + why_too_many_qpl_resp, data=df, FUN=length)
 #make_bar_plot(agg, x='why_too_many_qpl_resp', FALSE)
 make_pie_plot(agg, fill='why_too_many_qpl_resp', TRUE)
 remove(agg)
@@ -533,7 +533,7 @@ remove(agg)
 #
 # FIXME Verify why returning only 203 lines instead of 208 - Problem in the aggregate function
 plot_label('In your opinion, do you think that quantum developers would need yet another \nQuantum Programming Languages in the near future? Why?')
-agg <- aggregate(x=. ~ timestamp + why_need_another_qpl_resp, data=df, FUN=length)
+agg <- aggregate(x=country ~ timestamp + why_need_another_qpl_resp, data=df, FUN=length)
 #make_bar_plot(agg, x='why_need_another_qpl_resp', FALSE)
 make_pie_plot(agg, fill='why_need_another_qpl_resp', TRUE)
 remove(agg)
